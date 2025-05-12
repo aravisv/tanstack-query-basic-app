88ee57ba7f6e1d3770aaca8db274956e0f6d56a1
Aravinda S V <aravindasv@Aravindas-Laptop.local>
2025-05-12 19:57:42 +0530

    wrapped the app in queryClient. used the useQuery to create a query function and displayed the mock posts data

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Basic app using tanstack query aka react query
+
+Steps:
+
+1. installed @tanstack/react-query, @tanstack/react-query-devtools
+2. create a client like
+   const queryClient = new QueryClient();
+   then wrap the app with QueryClientProvider and pass the above created client
+3. create a query, pass queryKey and queryFn as 2 mandatory fields
+   use the isLoading, isError status first. if neither,then we can use the data to render
+   in case the async queryFn throws error, it will retry again few times, before the query's status - isError becomes true
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
 import "./App.css";
 
+const POSTS = [
+  {
+    id: 1,
+    title: "POST 1",
+  },
+  {
+    id: 2,
+    title: "POST 2",
+  },
+];
+
 function App() {
-  return <h1>Tan Stack Query</h1>;
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(2000).then(() => [...POSTS]),
+    // queryFn: () => {
+    //   console.log("calling the query function", Date.now());
+    //   return Promise.reject("Error");
+    // },
+  });
+  console.log({ postsQuery });
+  if (postsQuery.isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
+  console.log(postsQuery.data);
+
+  return (
+    <>
+      {postsQuery.data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </>
+  );
+}
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
